afbf9a5a44786a3cc3003726190d72b1e6a88095
Johan Blomgren <blommish@users.noreply.github.com>
2023-12-21 08:50:01 +0100

    Forenklet koden med å slitte ut metoder på en annen måte

--- src/post-repo-pr-status/repoStatus.ts
+++ src/post-repo-pr-status/repoStatus.ts
@@ -1,45 +1,17 @@
-import { octokit } from '../common/octokit';
-import { components } from '@octokit/openapi-types';
+import { hentPrs, hentRepos, hentReviews, Pr, PrMedReviews, Repo } from '../common/octokit';
 import { RepoStatus, Status } from './typer';
 
-const dotenv = require('dotenv');
-dotenv.config();
-
-type Repo = components['schemas']['repo-search-result-item'];
-type Pr = components['schemas']['pull-request'];
-type PrReview = components['schemas']['pull-request-review'];
-
-interface PrMedReviews {
-    pr: Pr;
-    reviews: PrReview[];
+interface RepoMedPrReviews {
+    repo: Repo;
+    prs: PrMedReviews[];
 }
 
-const headers = {
-    'X-GitHub-Api-Version': '2022-11-28',
+const initTotal: Status = {
+    antallGodkjente: 0,
+    antallVenter: 0,
+    antallUnderArbeid: 0,
+    antallDependabot: 0,
 };
-
-// Finner repos som er tagget med tilleggsstonader
-const hentRepos = () =>
-    octokit
-        .request('GET /search/repositories', {
-            q: 'owner:navikt+topic:tilleggsstonader',
-            headers: headers,
-        })
-        .then((res) => res.data.items as Repo[]);
-
-const hentPrs = (repo: Repo) =>
-    octokit
-        .request(`GET /repos/${repo.full_name}/pulls`, {
-            headers: headers,
-        })
-        .then((res) => ({ repo, prs: res.data as Pr[] }));
-
-const hentReviews = (repo: string, pr: Pr): Promise<PrMedReviews> =>
-    octokit
-        .request(`GET /repos/${repo}/pulls/${pr.number}/reviews`, {
-            headers: headers,
-        })
-        .then((res) => ({ pr, reviews: res.data as PrReview[] }));
 
 const erGodkjent = (pr: PrMedReviews) => pr.reviews.some((r) => r.state === 'APPROVED');
 const harReviews = (pr: PrMedReviews) => pr.reviews.length > 0;
@@ -48,29 +20,22 @@ const erIkkeGodkjent = (pr: PrMedReviews) => harReviews(pr) && !erGodkjent(pr);
 
 const erDependabot = (pr: Pr) => pr.user.login === 'dependabot[bot]';
 
-const hentRepoMedStatus = async (repo: Repo, prs: Pr[]): Promise<RepoStatus> => {
-    const brukerPrs = prs.filter((pr: Pr) => !erDependabot(pr));
-    const prMedReviews = await Promise.all(brukerPrs.map((pr) => hentReviews(repo.full_name, pr)));
+const tilRepoMedStatus = (repoMedPrs: RepoMedPrReviews): RepoStatus => {
+    const { repo, prs } = repoMedPrs;
+    const ikkeDependabotPrs = prs.filter((p) => !erDependabot(p.pr));
     return {
         navn: repo.name,
         pullsUrl: repo.pulls_url,
-        prs: prMedReviews.map((pr) => ({
+        prs: ikkeDependabotPrs.map((pr) => ({
             tittel: pr.pr.title,
             url: pr.pr.url,
             erGodkjent: erGodkjent(pr),
         })),
-        antallGodkjente: prMedReviews.filter(erGodkjent).length,
-        antallVenter: prMedReviews.filter(harIkkeReviews).length,
-        antallUnderArbeid: prMedReviews.filter(erIkkeGodkjent).length,
-        antallDependabot: prs.filter(erDependabot).length,
+        antallGodkjente: ikkeDependabotPrs.filter(erGodkjent).length,
+        antallVenter: ikkeDependabotPrs.filter(harIkkeReviews).length,
+        antallUnderArbeid: ikkeDependabotPrs.filter(erIkkeGodkjent).length,
+        antallDependabot: prs.filter((p) => erDependabot(p.pr)).length,
     };
-};
-
-const initTotal = {
-    antallGodkjente: 0,
-    antallVenter: 0,
-    antallUnderArbeid: 0,
-    antallDependabot: 0,
 };
 
 const beregnTotal = (repos: RepoStatus[]): Status =>
@@ -84,12 +49,32 @@ const beregnTotal = (repos: RepoStatus[]): Status =>
         initTotal
     );
 
+const hentPrsMedReviews = async (repo: Repo): Promise<PrMedReviews[]> => {
+    const repoPrs = await hentPrs(repo);
+    return Promise.all(
+        repoPrs.map(async (pr) => {
+            const reviews = erDependabot(pr) ? [] : await hentReviews(repo.full_name, pr.number);
+            return { pr, reviews };
+        })
+    );
+};
+
+const hentPrsTilRepos = (repos: Repo[]): Promise<RepoMedPrReviews[]> =>
+    Promise.all(
+        repos.map(async (repo) => {
+            const prs = await hentPrsMedReviews(repo);
+            return { repo, prs };
+        })
+    );
+
 export const hentRepoStatus = async () => {
-    const repos = await hentRepos();
-    const prs = await Promise.all(repos.map((repo) => hentPrs(repo)));
-    const statuser = await Promise.all(prs.map((pr) => hentRepoMedStatus(pr.repo, pr.prs)));
+    const repoMedStatus: RepoMedPrReviews[] = await hentRepos(
+        'owner:navikt+topic:tilleggsstonader'
+    ).then(hentPrsTilRepos);
+    const repos: RepoStatus[] = repoMedStatus.map((repo) => tilRepoMedStatus(repo));
+
     return {
-        repos: statuser,
-        total: beregnTotal(statuser),
+        repos: repos,
+        total: beregnTotal(repos),
     };
 };
